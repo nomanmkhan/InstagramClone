1bf1f5f6476c659eba449ee196f562572e0b7bc5
nomanmkhan <nomanmuhammadkhan@gmail.com>
2019-12-21 20:26:22 +0500

    Signup and Login Parse

--- app/src/main/java/com/example/apinstagramclone/SignUp.java
+++ app/src/main/java/com/example/apinstagramclone/SignUp.java
@@ -2,6 +2,7 @@ package com.example.apinstagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -94,6 +95,10 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         btnTransition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                Intent intent = new Intent(SignUp.this, SignUpLoginActivity.class);
+                startActivity(intent);
 
             }
         });
